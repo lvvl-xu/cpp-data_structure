039da0623bfce9d593354ceeef925ab2286be36a
lvvlxu <lvvl_xu@outlook.com>
2020-09-23 21:00:05 +0800

    更新

---
+++ cpp/cmake-build-debug/CMakeFiles/demo1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo1.dir/main.cpp.obj"
+  "demo1.exe"
+  "demo1.exe.manifest"
+  "demo1.pdb"
+  "libdemo1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
